8ebc1fd3dfbfd3b675cfd5b6f151646139a48922
cgjwolters <116182933+cgjwolters@users.noreply.github.com>
2024-11-04 21:45:13 +0100

    Added Trf3 and Vec3

---
+++ KinemaCSharp/Trf3.cs
@@ -0,0 +1,69 @@
+﻿public struct Trf3
+{
+  double[,] m = new double [3,4];
+
+  public bool isDerivative;
+
+  public void init()
+  {
+    m[0,0] = 1.0; m[0,1] = 0.0; m[0,2] = 0.0;
+    m[1,0] = 0.0; m[1,1] = 1.0; m[1,2] = 0.0;
+
+    isDerivative = false;
+  }
+
+  public void zero()                           // All elements to zero
+  {
+    m[0,0] = 0.0; m[0,1] = 0.0; m[0,2] = 0.0;
+    m[1,0] = 0.0; m[1,1] = 0.0; m[1,2] = 0.0;
+  
+    isDerivative = false;
+  }
+
+  public Trf3() { init(); }                      // Initialize to unitmat
+  public Trf3(double m00, double m01, double m02, double m03,
+              double m10, double m11, double m12, double m13,
+              double m20, double m21, double m22, double m23)
+  {
+    m[0,0] = m00; m[0,1] = m01; m[0,2] = m02;
+    m[1,0] = m10; m[1,1] = m11; m[1,2] = m12;
+
+    isDerivative = false;
+  }
+
+
+}
+
+Trf3(Vec3 org, Vec3 zDir, Vec3&xDir)
+{ 
+
+}
+
+double determinant() const;
+
+double scaleX() const;
+double scaleY() const;
+double scaleZ() const;
+
+bool mirrors() const { return determinant() < 0.0; } // Does it mirror?
+
+    void mirror(const Vec3& org, const Vec3& mirrorAxis); // Mirror along
+                                                          // axis
+
+bool invert() { return invertInto(*this); } // Invert transform
+bool invertInto(Trf3& invMat) const;        // Invert transform
+
+Trf3 & operator = (const Trf3& mt);          // Assignment
+
+Trf3 & operator += (const Trf3& mt);         // Add mt
+Trf3 & operator -= (const Trf3& mt);         // Subtract mt
+Trf3 & operator *= (double fact);            // Scaling
+
+Trf3 & operator *= (const Trf3& mt);         // Post multiply with mt
+Trf3 & preMultWith(const Trf3& mt);          // Pre multiply with mt
+
+Vec3 operator *(const Vec3& p) const;       // Transform Vec3
+
+double operator()(int ix, int iy) const;   // Return matrix element
+double& operator()(int ix, int iy);         // Return matrix el. ref.
+
---
+++ KinemaCSharp/Vec3.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace KinemaLibCs
+{
+  public struct Vec3
+  {
+    public double x, y, z;
+    bool isDerivative;
+
+    public Vec3(double x, double y, double z)
+    {
+      this.x = x; this.y = y; this.z = z;
+      isDerivative = false;
+    }
+
+    public double UnitLen3()        // Make unitlength, return old length
+    {
+      double ln = Len3();
+
+      if (ln > 0.0) {
+        x /= ln; y /= ln; z /= ln;
+      }
+
+      return ln;
+    }
+    
+    public double Len3() { return Math.Sqrt(x*x + y*y + z*z); }
+    public double LenSq3() { return x * x + y * y + z * z; }
+
+    public double Len3(double newLen)    // Set new length return old length;
+    {
+      double oldLen = Len3();
+
+      if (oldLen > 0.0) {
+        double fact = newLen / oldLen;
+        x *= fact; y *= fact; z *= fact;
+      }
+
+      return oldLen;
+    }
+
+    double DistTo3(Vec3 v)
+    {
+      Vec3 lv = new(v.x-x,v.y - y,v.z - z);
+
+      return lv.Len3();
+    }
+    double SqDistTo3(Vec3 v) // Square of distance
+    {
+      Vec3 lv = new(v.x - x, v.y - y, v.z - z);
+
+      return lv.x*lv.x + lv.y * lv.y + lv.z * lv.z; ;
+    }
+
+    double AngleTo3(Vec3 v) { return 0.0; }
+
+    double operator *(Vec3 v) { return x * v.x + y * v.y + z * v.z; }  // Inner product
+
+    Vec3 outer(Vec3 b) { } // Outer product (*this x b)
+
+    Vec3 operator *(double fact)
+    {
+      Vec3 lv = new(x,y,z);
+
+      lv.x *= fact; lv.y *= fact; lv.z *= fact;
+
+      return lv;
+    }
+    Vec3 operator /(double fact)
+    {
+      Vec3 lv = new(x, y, z);
+
+      lv.x /= fact; lv.y /= fact; lv.z /= fact;
+
+      return lv;
+    }
+
+    Vec3 operator +(Vec3 v) { }
+
+    Vec3 operator -(Vec3 v) { }
+
+    void operator *= (double fact) { x *= fact; y *= fact; z *= fact; }
+
+    void operator /= (double fact) { x /= fact; y /= fact; z /= fact; }
+
+    void operator += (Vec3 v) { x += v.x; y += v.y; z += v.z; }
+
+    void operator -= (Vec3 v) { x -= v.x; y -= v.y; z -= v.z; }
+
+  public override bool Equals(Object obj) { return true; }
+  public override int GetHashCode() { return 0; }
+
+  bool operator ==(const Vec3& v)
+  {
+    return distTo3(v) <= IdentDist;
+  }
+  bool operator !=(const Vec3& v)
+  {
+    return distTo3(v) > IdentDist;
+  }
+
+   void transform3(Trf3 trf)
+  {
+
+  }
+}
